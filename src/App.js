aca7aaffe9b2c0a1e50c2e355e19d4b5898a2584
Harshad Naik <naikharshad01@gmail.com>
2020-06-17 21:02:40 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { connect } from "react-redux";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    const { ct, onIncrement, onDecrement } = this.props;
+    return (
+      <div className="count">
+        <h1>
+          COUNT : <span>{ct}</span>
+        </h1>
+        <button onClick={onIncrement}>Increment</button>
+        <button onClick={onDecrement}>Decrement</button>
+      </div>
+    );
+  }
 }
+const mapStateToProps = (state) => {
+  return {
+    ct: state.count,
+  };
+};
 
-export default App;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onIncrement: () => dispatch({ type: "INCREMENT" }),
+    onDecrement: () => dispatch({ type: "DECREMENT" }),
+  };
+};
+export default connect(mapStateToProps, mapDispatchToProps)(App);
